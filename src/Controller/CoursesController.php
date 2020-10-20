97be0aac0bfd963de8bb7c8d1e0fe5eb2746d360
h-mochizuki <Nyamka>
2020-10-20 19:59:32 +0900

    page controller

--- src/Controller/CoursesController.php
+++ src/Controller/CoursesController.php
@@ -26,8 +26,7 @@ use Cake\View\Exception\MissingTemplateException;
  *
  * @link https://book.cakephp.org/3/en/controllers/pages-controller.html
  */
-class PagesController extends AppController
-{
+class CoursesController extends AppController  {
     /**
      * Displays a view
      *
@@ -38,35 +37,7 @@ class PagesController extends AppController
      *   be found
      * @throws \Cake\View\Exception\MissingTemplateException In debug mode.
      */
-    public function display(...$path)
-    {
-        if (!$path) {
-            return $this->redirect('/');
-        }
-        if (in_array('..', $path, true) || in_array('.', $path, true)) {
-            throw new ForbiddenException();
-        }
-        $page = $subpage = null;
-
-        if (!empty($path[0])) {
-            $page = $path[0];
-        }
-        if (!empty($path[1])) {
-            $subpage = $path[1];
-        }
-        $this->set(compact('page', 'subpage'));
-
-        try {
-            $this->render(implode('/', $path));
-        } catch (MissingTemplateException $exception) {
-            if (Configure::read('debug')) {
-                throw $exception;
-            }
-            throw new NotFoundException();
-        }
-    }
-
-    public function about()  {
+    public function display()  {
         
     }
 }
