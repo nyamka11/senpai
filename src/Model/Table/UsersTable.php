281997b26e823c542c8798a474cb26de9efb5ebf
nyamka <unyamka@gmail.com>
2020-11-09 21:35:13 +0900

    upadte

--- src/Model/Table/UsersTable.php
+++ src/Model/Table/UsersTable.php
@@ -161,11 +161,9 @@ class UsersTable extends Table
      * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
      * @return \Cake\ORM\RulesChecker
      */
-    public function buildRules(RulesChecker $rules)
-    {
+    public function buildRules(RulesChecker $rules)  {
         $rules->add($rules->isUnique(['username']));
         $rules->add($rules->isUnique(['email']));
-
         return $rules;
     }
 }
